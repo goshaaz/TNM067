8948fbbc4c05de90a88704eeab3eabe0699c1284
goshaaz <ligeorgij7@hotmail.com>
2021-02-01 23:15:23 +0100

    indentation

--- tnm067-labs-code-master/modules/tnm067lab4/jacobian.cpp
+++ tnm067-labs-code-master/modules/tnm067lab4/jacobian.cpp
@@ -8,7 +8,7 @@ mat2 jacobian(const ImageSampler& sampler, vec2 position, vec2 offset) {
     mat2 J(1); 
 	auto dVx = (sampler.sample(position.x + offset.x, position.y) - sampler.sample(position.x - offset.x, position.y)) / (2.0 * offset.x);
 	auto dVy = (sampler.sample(position.x, position.y + offset.y) - sampler.sample(position.x, position.y - offset.y)) / (2.0 * offset.y);
-    J[0][0] = dVx.x;
+	J[0][0] = dVx.x;
 	J[1][0] = dVy.x;
 	J[0][1] = dVx.y;
 	J[1][1] = dVy.y;
